3b67710e7c1a72770d8b24cf360738c797e61b64
Ryan Gill <rdgill95@gmail.com>
2018-12-27 02:42:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,8 @@
-#write your code here
-
-def countdown
+def countdown(seconds)
+  phrase = "#{seconds} SECOND(S)!"
+  while count > 0 do
+    puts phrase
+    count -= 1 
+  end
+  puts "HAPPY NEW YEAR!"
 end
